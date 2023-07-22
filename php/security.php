a14158e79504b926dd2d7a77cf0f25bec7d71ae8
vitorgabrieldev <vitorgabrieldeoliveiragit@gmail.com>
2023-07-22 10:04:49 -0300

    Add file - protect.php,logout.php - paste,phpSystem,phpWeb

--- php/security.php
+++ php/security.php
@@ -1,16 +1,10 @@
 <?php
 
-    $servername = "189.36.134.174";
+    $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "hub_work";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // if ($conn->connect_error) {
-    //     echo("Erro na conexão: " . $conn->connect_error);
-    // } else {
-    //     echo "Connection on System ( Hash : Cj6F9Ja4DkOpdGrT+2B ) -> State : { True };";
-    // };
 
 ?>
